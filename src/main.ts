c58d6845ad856682905dea2433cec8c0a04653af
Simon Campbell <simon.campbell@ultrafast.co.nz>
2022-12-11 16:10:25 +1300

    Add express

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import { greet } from "./Greeter";
 
+import express from 'express';
+
 function main(): void {
-  greet((message) => console.log(message));
+  const app = express();
+  const port = 3000;
+
+  app.get("/", (req: express.Request, res: express.Response) => {
+    greet((message) => res.json({ message }));
+  });
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
 }
 
 main();
